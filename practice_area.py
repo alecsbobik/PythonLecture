ab55f472ea6398a5a258a28c05d6e278b2e3bdaa
bobik <104901009+alecsbobik@users.noreply.github.com>
2024-10-11 11:42:27 +0800

    Add files via upload

---
+++ practice_area.py
@@ -0,0 +1,10 @@
+# number1 = 40
+# number2 = 30
+
+# The result is 70
+
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+average = number1 + number2
+print(f"The result is {average}")
